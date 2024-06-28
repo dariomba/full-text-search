b02240d5e592292a592fd63b818998e8930c69de
dariomba <dariomartinezbajo@gmail.com>
2024-06-28 19:34:14 +0200

    Extract mapping query to constants file

--- backend/src/cmd/main.go
+++ backend/src/cmd/main.go
@@ -53,64 +53,7 @@ func populateElastic(indexName string, records []map[string]interface{}) {
 }
 
 func createIndex(indexName string) {
-	mapping := `{
-		"settings": {
-			"number_of_shards": 1,
-			"number_of_replicas": 1,
-			"analysis": {
-				"analyzer": {
-					"autocomplete": {
-						"type": "custom",
-						"tokenizer": "standard",
-						"filter": ["lowercase", "autocomplete_filter"]
-					}
-				},
-				"filter": {
-					"autocomplete_filter": {
-						"type": "edge_ngram",
-						"min_gram": 1,
-						"max_gram": 20
-					}
-				}
-			}
-		},
-		"mappings": {
-			"properties": {
-				"Release_Date": {
-					"type": "date",
-					"format": "yyyy-MM-dd"
-				},
-				"Title": {
-					"type": "text",
-					"analyzer": "autocomplete",
-					"search_analyzer": "standard"
-				},
-				"Overview": {
-					"type": "text",
-					"analyzer": "standard"
-				},
-				"Popularity": {
-					"type": "float"
-				},
-				"Vote_Count": {
-					"type": "integer"
-				},
-				"Vote_Average": {
-					"type": "float"
-				},
-				"Original_Language": {
-					"type": "keyword"
-				},
-				"Genre": {
-					"type": "keyword"
-				},
-				"Poster_Url": {
-					"type": "keyword",
-					"index": false
-				}
-			}
-		}
-	}`
+	mapping := constants.MappingIndexQuery
 
 	req := esapi.IndicesCreateRequest{
 		Index: indexName,
--- backend/src/internal/constants/constants.go
+++ backend/src/internal/constants/constants.go
@@ -1,6 +1,64 @@
 package constants
 
 const (
-	IndexName   = "movies"
-	CsvFilename = "movies.csv"
+	IndexName         = "movies"
+	CsvFilename       = "movies.csv"
+	MappingIndexQuery = `{
+		"settings": {
+			"number_of_shards": 1,
+			"number_of_replicas": 1,
+			"analysis": {
+				"analyzer": {
+					"autocomplete": {
+						"type": "custom",
+						"tokenizer": "standard",
+						"filter": ["lowercase", "autocomplete_filter"]
+					}
+				},
+				"filter": {
+					"autocomplete_filter": {
+						"type": "edge_ngram",
+						"min_gram": 1,
+						"max_gram": 20
+					}
+				}
+			}
+		},
+		"mappings": {
+			"properties": {
+				"Release_Date": {
+					"type": "date",
+					"format": "yyyy-MM-dd"
+				},
+				"Title": {
+					"type": "text",
+					"analyzer": "autocomplete",
+					"search_analyzer": "standard"
+				},
+				"Overview": {
+					"type": "text",
+					"analyzer": "standard"
+				},
+				"Popularity": {
+					"type": "float"
+				},
+				"Vote_Count": {
+					"type": "integer"
+				},
+				"Vote_Average": {
+					"type": "float"
+				},
+				"Original_Language": {
+					"type": "keyword"
+				},
+				"Genre": {
+					"type": "keyword"
+				},
+				"Poster_Url": {
+					"type": "keyword",
+					"index": false
+				}
+			}
+		}
+	}`
 )
