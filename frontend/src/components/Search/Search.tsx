62a426095f23da7ef0aec2b83b7dcaa47b69033c
dariomba <dariomartinezbajo@gmail.com>
2024-06-29 20:52:43 +0200

    Add search with suggestions first approach

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -10,12 +10,9 @@ export const Search: React.FC = () => {
     const searchParams = new URLSearchParams(window.location.search);
     return searchParams.get('q') || '';
   });
+  const [showMovies, setShowMovies] = useState(false);
   const [movies, setMovies] = useState<Movie[]>([]);
   const debounceSearch = useDebounce(search, DEBOUNCE_DELAY);
-
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
-  };
 
   useEffect(() => {
     if (debounceSearch === '') {
@@ -43,39 +40,81 @@ export const Search: React.FC = () => {
     }
   }
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setShowMovies(false);
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setShowMovies(true);
+  };
+
+  const handleClickSuggestion = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    const clickedElement = event.currentTarget.textContent;
+    if (clickedElement) {
+      setSearch(clickedElement);
+      setTimeout(() => {
+        setShowMovies(true);
+      }, DEBOUNCE_DELAY * 2);
+    }
+  };
+
   return (
     <>
-      <form>
-        <div className="relative">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <div className="flex">
+          <div className="relative w-full">
+            <input
+              type="search"
+              id="default-search"
+              onChange={handleSearch}
+              value={search}
+              className="flex w-full p-4 text-sm border rounded-2xl bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:border-gray-500"
+              placeholder="Avatar, The Matrix, Inception..."
+            />
+            <button
+              type="submit"
+              className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white rounded-e-lg border border-blue-700  focus:ring-4 focus:outline-none  bg-blue-600 hover:bg-blue-700 focus:border-gray-500"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span className="sr-only">Search</span>
+            </button>
+            {!showMovies && movies.length > 0 && (
+              <ul
+                id="autocomplete-results"
+                className="absolute z-10 w-1/2 bg-gray-700 border border-gray-600 rounded-xl mt-1 p-4"
+              >
+                {movies.map((movie) => (
+                  <li
+                    key={`suggestion-movie-${movie.ID}`}
+                    className="text-white cursor-pointer hover:bg-gray-800 hover:rounded"
+                    onClick={handleClickSuggestion}
+                  >
+                    {movie.Title}
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
-          <input
-            type="search"
-            id="default-search"
-            onChange={handleSearch}
-            defaultValue={search}
-            className="block w-full p-4 ps-10 text-sm border rounded-2xl bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:border-gray-500"
-            placeholder="Avatar, The Matrix, Inception..."
-          />
         </div>
       </form>
 
-      <Movies movies={movies} />
+      {showMovies && <Movies movies={movies} />}
     </>
   );
 };
