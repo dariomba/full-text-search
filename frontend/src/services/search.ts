5e3fa9fceccfd0e2250d659ec9690538b8273c0e
dariomba <dariomartinezbajo@gmail.com>
2024-07-02 16:29:50 +0200

    Project dockerized and working!

--- frontend/src/services/search.ts
+++ frontend/src/services/search.ts
@@ -15,8 +15,10 @@ export interface Movie {
   Vote_Count: string;
 }
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+
 export const searchAPI = async (query: string) => {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/search?q=${query}`);
+  const res = await fetch(`${API_URL}/search?q=${query}`);
   const moviesResponse = (await res.json()) as SearchResponse;
   return moviesResponse.movies;
 };
